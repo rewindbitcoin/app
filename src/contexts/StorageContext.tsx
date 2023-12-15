13eacb55f969d9d6c3b79520d68319be09fc509d
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-12-15 11:04:52 +0100

    storage: share state across components

--- src/contexts/StorageContext.tsx
+++ src/contexts/StorageContext.tsx
@@ -7,40 +7,21 @@ import React, {
 } from 'react';
 import { storage } from '../lib/storage';
 
-type FactoryType = <T>(key: string) => {
-  value: T | undefined;
-  setValue: React.Dispatch<React.SetStateAction<T | undefined>>;
-  isStorageSynchd: boolean;
+//
+
+type StorageState<T> = Record<string, { isSynchd: boolean; value: T }>;
+type ProviderValue<T> = {
+  storageState: StorageState<T>;
+  setStorageState: React.Dispatch<React.SetStateAction<StorageState<T>>>;
 };
-const StorageContext = createContext<FactoryType | null>(null);
+const StorageContext = createContext<ProviderValue<unknown> | null>(null);
 
 const StorageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  console.log('StorageProvider');
-  const factory = <T,>(key: string) => {
-    console.log(`Creating setters for key ${key}`);
-    const [value, setValue] = useState<T | undefined>();
-    const [isStorageSynchd, setIsStorageSynchd] = useState(false);
+  //console.log('StorageProvider');
+  const [storageState, setStorageState] = useState<StorageState<unknown>>({});
 
-    useEffect(() => {
-      const fetchValue = async () => {
-        const savedValue = await storage.getStringAsync(key);
-
-        if (typeof savedValue === 'string') {
-          try {
-            const parsedValue = JSON.parse(savedValue);
-            setValue(parsedValue as T);
-          } catch (error) {
-            setValue(savedValue as T);
-          }
-        }
-        setIsStorageSynchd(true);
-      };
-      fetchValue();
-    }, []);
-    return { value, setValue, isStorageSynchd };
-  };
   return (
-    <StorageContext.Provider value={factory}>
+    <StorageContext.Provider value={{ storageState, setStorageState }}>
       {children}
     </StorageContext.Provider>
   );
@@ -53,19 +34,53 @@ const useStorage = <T,>(
   if (context === null) {
     throw new Error(`useStorage must be used within a StorageProvider`);
   }
+  const { storageState, setStorageState } = context;
 
-  const factory = context;
-  const { value, setValue, isStorageSynchd } = factory<T>(key);
+  useEffect(() => {
+    //We only need to retrieve the value from Storage intially (when isSynchd
+    //is not defined). After that, then we will rely on storageState[key].value
+    //to not spam the storage with more requests that we already know the result
+    if (!storageState[key]?.isSynchd) {
+      //console.log(`fetching key ${key}`);
+      const fetchValue = async () => {
+        const savedValue = await storage.getStringAsync(key);
+        let parsedValue: T | undefined;
+
+        if (typeof savedValue === 'string') {
+          try {
+            parsedValue = JSON.parse(savedValue);
+          } catch (error) {
+            parsedValue = savedValue as T;
+          }
+        }
+        setStorageState(prevState => {
+          //console.log(`setting isSynchd to true for ${key}`);
+          return {
+            ...prevState,
+            [key]: { value: parsedValue, isSynchd: true }
+          };
+        });
+      };
+      fetchValue();
+    }
+  }, [storageState, key, storage]);
 
   const setStorageValue = async (newValue: T) => {
     await storage.setStringAsync(
       key,
       typeof newValue === 'string' ? newValue : JSON.stringify(newValue)
     );
-    setValue(newValue);
+    setStorageState(prevState => ({
+      ...prevState,
+      [key]: { value: newValue, isSynchd: true }
+    }));
   };
 
-  return [value, setStorageValue, isStorageSynchd];
+  return [
+    storageState[key]?.value as T | undefined,
+    setStorageValue,
+    !!storageState[key]?.isSynchd
+  ];
 };
 
 export { StorageProvider, useStorage };
