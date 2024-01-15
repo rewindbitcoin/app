19367c87e61ba8d94cb41b7cadbb98369ffac5e5
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-01-15 08:03:17 +0100

    comments: add brief explanation how to activate reanimated 3 plugins

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo']
+    //plugins below needed for react-native-reanimated:
     //should go with:
     // npx expo install @babel/plugin-proposal-export-namespace-from react-native-reanimated
     //plugins: [
