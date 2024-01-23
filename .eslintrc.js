658b25fdcdb7f7c992ed84039af995b04767cc18
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-01-23 12:11:34 +0100

    linter: add react useEffect and useCallback linters for dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    './node_modules/@bitcoinerlab/configs/eslintConfig',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    // Add any specific rules you want to override or enforce
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+    // ...other custom rules
+  },
+  settings: {
+    react: {
+      version: 'detect' // Automatically detect the React version
+    }
+  }
+};
