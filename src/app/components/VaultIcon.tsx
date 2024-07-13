3695993f92738ef87ad4dea9c79cd09be08903c9
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-07-13 20:21:40 +0200

    fix ios round icon

--- src/app/components/VaultIcon.tsx
+++ src/app/components/VaultIcon.tsx
@@ -19,12 +19,12 @@ const VaultIcon = ({
   ) : remainingBlocks === 'VAULT_NOT_FOUND' ? (
     <MaterialCommunityIcons name="snowflake-off" size={24} color="red" />
   ) : remainingBlocks === 'TRIGGER_NOT_PUSHED' ? (
-    <Svg
-      className="fill-white w-6 h-6 bg-primary p-0.5 rounded-full bg-[#4286E7]"
-      viewBox="0 0 24 24"
-    >
-      <FreezeIcon />
-    </Svg>
+    //bg-[#4286E7]
+    <View className="rounded-full w-6 h-6 p-0.5 overflow-hidden bg-primary">
+      <Svg className="fill-white" viewBox="0 0 24 24">
+        <FreezeIcon />
+      </Svg>
+    </View>
   ) : remainingBlocks === 'SPENT_AS_PANIC' ? (
     <MaterialCommunityIcons name="alarm-light" size={24} color="red" />
   ) : remainingBlocks === 'SPENT_AS_HOT' || remainingBlocks === 0 ? (
@@ -32,46 +32,11 @@ const VaultIcon = ({
       <HotIcon />
     </Svg>
   ) : (
-    <View className="flex-row items-center">
-      <Svg
-        className="fill-white w-6 h-6 p-0.5 rounded-full bg-[#800080]"
-        viewBox="0 0 24 24"
-      >
+    <View className="flex-row items-center rounded-full w-6 h-6 p-0.5 overflow-hidden bg-[#800080]">
+      <Svg className="fill-white" viewBox="0 0 24 24">
         <UnfreezeIcon />
       </Svg>
     </View>
   );
 };
-
-/*
- *
- * old rescue save buoy:
- * import SaveBuoy from '../../../assets/SaveBuoy.svg';
- *     <SaveBuoy className="w-6 h-6" />
- */
-
-/* old unfreeze icon:
-import UnfreezeIcon from './UnfreezeIcon';
-    <Svg
-      className="fill-white w-6 h-6 p-0.5 rounded-full bg-[#6baff9]"
-      viewBox="0 0 24 24"
-    >
-      <UnfreezeIcon />
-    </Svg>
-    */
-/*
- * unfreeze sequence:
-  *
-      <Svg
-        className="mr-1.5 fill-white w-6 h-6 bg-primary p-0.5 rounded-full bg-[#4286E7]"
-        viewBox="0 0 24 24"
-      >
-        <FreezeIcon />
-      </Svg>
-      <FontAwesome name="long-arrow-right" size={10} color="black" />
-      <Svg className="ml-1 fill-yellow-400 w-8 h-8" viewBox="0 0 24 24">
-        <HotIcon />
-      </Svg>
-      */
-
 export default React.memo(VaultIcon);
