ec92ea29b98308987fbffd43cd50c628000ab1b4
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-07-19 12:24:15 +0200

    new hook and provider informing about network status (general internet reachability / api reachability)

---
+++ src/common/hooks/useNetStatus.ts
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { NetStatusContext, NetStatus } from '../contexts/NetStatusContext';
+
+export const useNetStatus = (): NetStatus => {
+  const context = useContext(NetStatusContext);
+  if (!context) {
+    throw new Error('useNetStatus must be used within a NetStatusProvider');
+  }
+  return context;
+};
