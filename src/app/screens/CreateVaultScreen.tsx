5a3d94dbb6b285c1770c96ba2ff1634c6460894b
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-08-28 20:58:32 +0200

    CreateVaultScreen - handle more cases, make use of netRequest and handle errors. Added more TODOs

--- src/app/screens/CreateVaultScreen.tsx
+++ src/app/screens/CreateVaultScreen.tsx
@@ -1,3 +1,8 @@
+//TODO: test what happens if the user disables wifi and then clicks on create vault
+//  It shoudl fail for not being able to get servcieAddress and goHome after toas
+//  Problem is i get the toast for the lack of connectivity plus the error:
+//  errorMessage: app.fetchServiceAddressError
+//TODO: test what happens if the user disables wifi and then clicks on confirm
 import React, {
   useEffect,
   useState,
@@ -22,15 +27,14 @@ import { p2pBackupVault, fetchP2PVaultIds } from '../lib/backup';
 import { useNavigation } from '@react-navigation/native';
 import { useNetStatus } from '../hooks/useNetStatus';
 import { NavigationPropsByScreenId, WALLET_HOME } from '../screens';
+import { batchedUpdates } from '~/common/lib/batchedUpdates';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 export default function CreateVaultScreen({
-  vaultSettings,
-  onVaultPushed
+  vaultSettings
 }: {
   vaultSettings: VaultSettings | undefined;
-  onVaultPushed: () => void;
 }) {
   if (!vaultSettings) throw new Error('vaultSettings not set');
   const { amount, feeRate, lockBlocks, coldAddress } = vaultSettings;
@@ -47,7 +51,7 @@ export default function CreateVaultScreen({
     getChangeDescriptor,
     getUnvaultKey,
     signers,
-    pushVaultAndUpdateStates,
+    vaultPushAndUpdateStates,
     vaults,
     vaultsAPI,
     vaultsSecondaryAPI,
@@ -59,7 +63,7 @@ export default function CreateVaultScreen({
     !utxosData ||
     !networkId ||
     !signers ||
-    !pushVaultAndUpdateStates ||
+    !vaultPushAndUpdateStates ||
     !vaultsAPI ||
     !vaultsSecondaryAPI
   )
@@ -77,11 +81,16 @@ export default function CreateVaultScreen({
     );
   // We know settings are the correct ones in this Component
   const [progress, setProgress] = useState<number>(0);
+  const [confirmRequested, setConfirmRequested] = useState<boolean>(false);
   const [vault, setVault] = useState<Vault>();
-  const onProgress = useCallback((progress: number) => {
-    setProgress(progress);
-    return keepProgress.current;
-  }, []);
+  const onProgress = useCallback(
+    (progress: number) => {
+      setProgress(progress);
+      if (!navigation.isFocused()) keepProgress.current = false;
+      return keepProgress.current;
+    },
+    [navigation]
+  );
   const samples = settings.SAMPLES;
   const feeRateCeiling = settings.PRESIGNED_FEE_RATE_CEILING;
 
@@ -94,17 +103,104 @@ export default function CreateVaultScreen({
     //WALLETS
     navigation.navigate(WALLET_HOME, { walletId });
   }, [navigation, walletId]);
-  useEffect(() => {
-    if (!apiReachable || !api2Reachable) goWalletHome();
-  }, [apiReachable, api2Reachable, goWalletHome]);
+
+  //TODO: this below is a problem if the internet suddenly disconnects
+  //while doing some vault progress. the user is completelly disoriened
+  //useEffect(() => {
+  //  if (!apiReachable || !api2Reachable) goWalletHome();
+  //}, [apiReachable, api2Reachable, goWalletHome]);
 
   const isVaultCreated = useRef<boolean>(false);
+
+  const signer = signers[0];
+  if (!signer) throw new Error('signer unavailable');
+
+  const confirm = useCallback(async () => {
+    setConfirmRequested(true);
+    if (!vault) throw new Error('Unset vault cannot be confirmed');
+    //TODO: here now also show the progress, also it this fails then do
+    //not proceed
+    //TODO: Also there is a vaultPushAndUpdateStates that does stuff. integrate
+    //both
+    //TODO: this status must be checked to see if continue
+    const { result: backedUp, status: backupStatus } = await netRequest({
+      func: () =>
+        p2pBackupVault({
+          vault,
+          signer,
+          vaultsAPI,
+          vaultsSecondaryAPI,
+          onProgress,
+          networkId
+        }),
+      requirements: { apiReachable: true, api2Reachable: true },
+      errorMessage: t('createVault.p2pBackupVaultError')
+    });
+    if (backupStatus !== 'SUCCESS') {
+      //requirements not met or failure. Errors are toasted already.
+      //TODO: when !requirements is not met then the toast may be shown in a
+      //completelly different moment... maybe i should toast even more
+      goBack();
+      return;
+    }
+    if (backedUp === false && backupStatus === 'SUCCESS') {
+      //This means the user cancelled the backup process by stopping the
+      //compression using onProgress
+      goBack();
+      return;
+    }
+    //This means the screen is not focussed anymore!?!?!
+    //Don't proceed.
+    if (!navigation.isFocused()) return;
+
+    setProgress(1);
+    //Pushes the vault and then updates:
+    //  - Vaults and VaultsStatuses, discoveryExport local storage and
+    //  - also derived data: utxosData and historyData
+    const { status: pushAndUpdateStatus } = await netRequest({
+      func: () => vaultPushAndUpdateStates(vault),
+      requirements: { apiReachable: true },
+      //TODO: en.js update with createVault.p2pPushVaultError - explain that no
+      //sweats here since the backup worked fine and if the
+      //tx is pushed you're just fine. If not, retry.
+      errorMessage: t('createVault.p2pPushVaultError')
+    });
+
+    if (pushAndUpdateStatus === 'SUCCESS') {
+      toast.show(t('createVault.vaultSuccess'), { type: 'success' });
+      goWalletHome();
+    } else {
+      //requirements not met or failure. Errors are toasted already.
+      //TODO: when !requirements is not met then the toast may be shown in a
+      //completelly different moment... maybe i should toast even more
+      goBack();
+    }
+  }, [
+    goWalletHome,
+    toast,
+    signer,
+    netRequest,
+    vault,
+    vaultsAPI,
+    vaultsSecondaryAPI,
+    onProgress,
+    networkId,
+    t,
+    navigation,
+    goBack,
+    vaultPushAndUpdateStates
+  ]);
   useEffect(() => {
     if (isVaultCreated.current === true) return;
     else isVaultCreated.current = true;
 
+    if (!apiReachable || !api2Reachable) {
+      goWalletHome();
+      return;
+    }
+
     //Leave some time so that the progress is rendered
-    const createAndNotifyVault = async () => {
+    const create = async () => {
       await sleep(200);
       if (!navigation.isFocused()) return; //Don't proceed if lost focus after await
 
@@ -122,9 +218,7 @@ export default function CreateVaultScreen({
       const changeDescriptor = await getChangeDescriptor();
       if (!navigation.isFocused()) return; //Don't proceed if lost focus after await
 
-      const signer = signers[0];
-      if (!signer) throw new Error('signer unavailable');
-
+      //TODO:: fetchP2PVaultIds should not be in a netRequest??? its a fetch!
       const { nextVaultId, nextVaultPath } = await fetchP2PVaultIds({
         signer,
         networkId,
@@ -151,49 +245,28 @@ export default function CreateVaultScreen({
         nextVaultPath,
         onProgress
       });
-      if (!navigation.isFocused()) return; //Don't proceed if lost focus after await
-
-      //TODO: ask for confirmation, then:
-      if (typeof vault === 'object') {
-        setVault(vault);
-        //TODO: here now also show the progress, also it this fails then do
-        //not proceed
-        //TODO: Also there is a pushVaultAndUpdateStates that does stuff. integrate
-        //both
-        //TODO: this status must be checked to see if continue
-        await netRequest({
-          func: () =>
-            p2pBackupVault({
-              vault,
-              signer,
-              vaultsAPI,
-              vaultsSecondaryAPI,
-              onProgress,
-              networkId
-            }),
-          requirements: { apiReachable: true, api2Reachable: true },
-          errorMessage: t('createVault.p2pBackupVaultError')
-        });
-
-        if (!navigation.isFocused()) return; //Don't proceed after async op if unmounted - TODO: here we backup up the vault but then never pushed it, shoudn't the push be done by Rewind servers?
-
-        //Updates Vaults And VaultsStatuses local storage
-        await pushVaultAndUpdateStates(vault); //TODO: netRequest this shit because the push may have failed - even if this failed, note that
-        //TODO: if failed then run a modal (don't toas since this is important)
-        //and explain that no sweats here since the backup worked fine and if the
-        //tx is pushed you're just fine. If not, retry.
-        //TODO: toast succesfully backup up and pushed.
-        onVaultPushed();
-      } else {
-        if (vault !== 'USER_CANCEL') {
-          const errorMessage = t('createVault.error', { message: vault });
-          toast.show(errorMessage, { type: 'danger' });
+      //Don't proceed if lost focus after await
+      if (navigation.isFocused()) {
+        if (typeof vault === 'object') {
+          batchedUpdates(() => {
+            setVault(vault);
+            setProgress(0);
+          });
+        } else {
+          if (vault !== 'USER_CANCEL') {
+            //TODO: does this toast make sense goven the other notifications??
+            const errorMessage = t('createVault.error', { message: vault });
+            toast.show(errorMessage, { type: 'danger' });
+          }
+          //TODO: goBack here? i think so but make surem this is for usercancel
+          goBack();
         }
-        goBack();
       }
     };
-    createAndNotifyVault();
+    create();
   }, [
+    apiReachable,
+    api2Reachable,
     navigation,
     goWalletHome,
     netRequest,
@@ -210,16 +283,19 @@ export default function CreateVaultScreen({
     lockBlocks,
     networkId,
     onProgress,
-    onVaultPushed,
-    pushVaultAndUpdateStates,
+    vaultPushAndUpdateStates,
     samples,
     vaultsAPI,
     vaultsSecondaryAPI,
     settings.SERVICE_FEE_RATE,
-    signers,
+    signer,
     vaults,
     utxosData
   ]);
+
+  const stopProgress = useCallback(() => {
+    keepProgress.current = false;
+  }, []);
 
   return (
     <KeyboardAwareScrollView
@@ -230,13 +306,35 @@ export default function CreateVaultScreen({
       <View className="self-center pt-5 max-w-lg w-full mx-4" style={mbStyle}>
         {vault ? (
           <>
-            <Button
-              onPress={() => {
-                keepProgress.current = false;
-              }}
-            >
-              {t('confirmButton')}
-            </Button>
+            {progress !== 1 ? (
+              <>
+                <Text>
+                  TODO: backing up the vault and checlking the backup is ok
+                </Text>
+                <View className="flex-grow justify-center items-center">
+                  <Progress.Circle
+                    size={300}
+                    showsText={true}
+                    progress={progress}
+                  />
+                </View>
+              </>
+            ) : (
+              <Text>TODO: hold on a bit more here. pushing the vault...</Text>
+            )}
+            {!confirmRequested ? (
+              <>
+                <Button onPress={goBack}>{t('cancelButton')}</Button>
+                <Button onPress={confirm}>{t('confirmButton')}</Button>
+              </>
+            ) : (
+              progress !== 1 && (
+                //when progress is 1 this means the backups has been done
+                //and now it's pushing the vault. Don't let the user
+                //cancel at this stage!!!
+                <Button onPress={stopProgress}>{t('cancelButton')}</Button>
+              )
+            )}
           </>
         ) : (
           <>
@@ -246,26 +344,14 @@ export default function CreateVaultScreen({
             <Text style={{ marginVertical: 20, alignSelf: 'flex-start' }}>
               {t('createVault.intro')}
             </Text>
-            <View
-              style={{
-                flexGrow: 1,
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-            >
+            <View className="flex-grow justify-center items-center">
               <Progress.Circle
                 size={300}
                 showsText={true}
                 progress={progress}
               />
             </View>
-            <Button
-              onPress={() => {
-                keepProgress.current = false;
-              }}
-            >
-              {t('cancelButton')}
-            </Button>
+            <Button onPress={stopProgress}>{t('cancelButton')}</Button>
           </>
         )}
       </View>
