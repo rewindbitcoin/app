8241dbe5e584668b9880d477792bb9c51b18ed3f
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-09-05 15:54:11 +0200

    New icons

---
+++ src/app/components/SendIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Path } from 'react-native-svg';
+const SendIcon = () => (
+  <Path d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+);
+
+export default SendIcon;
