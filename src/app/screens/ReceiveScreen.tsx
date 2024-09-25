abb4b96fc658c7eafd846ffe98bb809a831c053c
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-09-25 14:34:19 +0200

    ui: colors

--- src/app/screens/ReceiveScreen.tsx
+++ src/app/screens/ReceiveScreen.tsx
@@ -195,7 +195,7 @@ export default function Receive() {
           </View>
           <Button onPress={goBack}>{t('receive.doneButton')}</Button>
           {requestTokensURL && networkName && (
-            <View className="mt-4 p-4 bg-gray-100 shadow rounded-lg items-center">
+            <View className="mt-4 p-4 bg-gray-50 shadow rounded-lg items-center">
               <View className="flex-row items-center mb-2">
                 <Text className="text-center">{t('receive.faucetIntro')}</Text>
                 <Button mode="text" onPress={onFaucet} className="ml-2">
