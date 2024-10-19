27801c9110c0b78722855ff5b43bfbc57a7736c9
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-10-19 12:46:03 +0200

    translations: don't use partial translations

--- src/i18n-locales/es.js
+++ src/i18n-locales/es.js
@@ -1,15 +1,15 @@
 //Treat this as a golrified json with comments & multi-line using string literals
 export default {
-  vaultSetup: {
-    amountLabel: 'Cantidad:',
-    securityLockTimeLabel: 'Tiempo de Bloqueo de Seguridad (bloques):',
-    securityLockTimeDescription: 'Disponible {{blocks}} después de Desbloquear',
-    confirmationSpeedLabel: 'Tiempo de Confirmación:',
-    okButton: 'Aceptar',
-    saveButton: 'Guardar',
-    cancelButton: 'Cancelar',
-    invalidValuesTitle: 'Valores Inválidos',
-    invalidValuesMessage:
-      'Se han proporcionado valores inválidos. Por favor, corrige los siguientes errores:\n\n{{errorMessages}}'
-  }
+  //vaultSetup: {
+  //  amountLabel: 'Cantidad:',
+  //  securityLockTimeLabel: 'Tiempo de Bloqueo de Seguridad (bloques):',
+  //  securityLockTimeDescription: 'Disponible {{blocks}} después de Desbloquear',
+  //  confirmationSpeedLabel: 'Tiempo de Confirmación:',
+  //  okButton: 'Aceptar',
+  //  saveButton: 'Guardar',
+  //  cancelButton: 'Cancelar',
+  //  invalidValuesTitle: 'Valores Inválidos',
+  //  invalidValuesMessage:
+  //    'Se han proporcionado valores inválidos. Por favor, corrige los siguientes errores:\n\n{{errorMessages}}'
+  //}
 };
