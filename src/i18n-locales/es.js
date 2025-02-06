44c70f8c9c6bb6b864c0eb33a2351d13dd971a81
José Luis Landabaso Díaz <landabaso@gmail.com>
2025-02-06 11:16:44 +0100

    docs: Update Spanish translations to use "billetera" consistently

--- src/i18n-locales/es.js
+++ src/i18n-locales/es.js
@@ -6,10 +6,10 @@ export default {
     syncP2PVaultsError: `No se pudo conectar a la red de respaldo P2P. Este problema impide sincronizar las bóvedas creadas en otros dispositivos.
 
 {{message}}`,
-    syncNetworkError: `Hubo un problema durante una solicitud de red al actualizar tu cartera.
+    syncNetworkError: `Hubo un problema durante una solicitud de red al actualizar tu billetera.
 
 {{message}}`,
-    syncUnexpectedError: `Ha ocurrido un problema inesperado al actualizar tu cartera.
+    syncUnexpectedError: `Ha ocurrido un problema inesperado al actualizar tu billetera.
 
 {{message}}`,
     pushError: `¡Ups! Hubo un problema al enviar tu transacción. Por favor, verifica tu conexión e inténtalo de nuevo.
@@ -27,13 +27,13 @@ export default {
     btcRatesError:
       'No se pudieron obtener el cambio actual de BTC. Los montos mostrados en {{currency}} pueden no ser precisos.',
     secureStorageAuthenticationPrompt:
-      'Por favor, autentícate para acceder a los datos sensibles de tu cartera.',
+      'Por favor, autentícate para acceder a los datos sensibles de tu billetera.',
     secureStorageCreationAuthenticationPrompt:
-      'Por favor, autentícate para crear de forma segura una nueva cartera y cifrar tus datos sensibles.'
+      'Por favor, autentícate para crear de forma segura una nueva billetera y cifrar tus datos sensibles.'
   },
   netStatus: {
     internetNotReachableWarning:
-      'No se detecta conexión a Internet. Esto impide verificar el estado de la cartera, gestionar bóvedas y enviar o recibir fondos.',
+      'No se detecta conexión a Internet. Esto impide verificar el estado de la billetera, gestionar bóvedas y enviar o recibir fondos.',
     apiNotReachableWarning:
       'No se puede conectar a nuestros servicios. Esto afecta a las actualizaciones del precio de Bitcoin e impide operaciones con bóvedas debido a la interrupción de las copias de seguridad.',
     communityBackupsdNotReachableWarning:
@@ -43,6 +43,6 @@ export default {
     blockchainMainnetExplorerNotReachableWarning:
       'No se puede conectar al explorador de la blockchain. Esto impide actualizar el estado de tus transacciones y acceder a información actualizada de la red Bitcoin.',
     connectionRestoredInfo:
-      'Conectividad restaurada. Todas las funciones de la cartera están ahora completamente operativas.'
+      'Conectividad restaurada. Todas las funciones de la billetera están ahora completamente operativas.'
   }
 };
