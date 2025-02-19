ecc6dd16d24f9c42d165df4ad7e695ca7463515c
José Luis Landabaso Díaz <landabaso@gmail.com>
2025-02-19 12:45:55 +0100

    style: Adjust button gap for responsive layout

--- src/app/components/ConfirmBip39.tsx
+++ src/app/components/ConfirmBip39.tsx
@@ -81,7 +81,7 @@ const ConfirmBip39: React.FC<ConfirmBip39Props> = ({
       {...(isConfirming || isSkipping ? {} : { onClose: onCancel })}
       customButtons={
         <>
-          <View className="items-center gap-6 flex-row flex-wrap justify-center">
+          <View className="items-center gap-4 moblg:gap-6 flex-row flex-wrap justify-center">
             <Button disabled={isConfirming || isSkipping} onPress={onCancel}>
               {t('cancelButton')}
             </Button>
