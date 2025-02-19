8e966caad34180b1e51ceda54f6dc4c42c10996e
José Luis Landabaso Díaz <landabaso@gmail.com>
2025-02-19 17:22:02 +0100

    refactor: Move safe area insets from Button to WalletButtons component

--- src/app/components/WalletButtons.tsx
+++ src/app/components/WalletButtons.tsx
@@ -16,13 +16,11 @@ const Button = ({
   type: 'SEND' | 'RECEIVE' | 'FREEZE';
   onLayout?: (e: LayoutChangeEvent) => void;
 }) => {
-  const insets = useSafeAreaInsets();
   const { t } = useTranslation();
   return (
     <Pressable
       onPress={onPress}
       onLayout={onLayout}
-      style={{ marginBottom: insets.bottom }}
       className={
         'py-2.5 pl-2.5 pr-3.5 mobmed:py-4 mobmed:pl-4 mobmed:pr-5 bg-primary rounded-full hover:opacity-90 active:scale-95 active:opacity-90 shadow flex-row gap-1 justify-center items-center'
       }
@@ -65,6 +63,7 @@ const WalletButtons = ({
 }) => {
   const [buttonHeight, setButtonHeight] = useState(0);
   const [isMultiRow, setIsMultiRow] = useState(false);
+  const insets = useSafeAreaInsets();
   const elCount =
     (handleReceive ? 1 : 0) + (handleSend ? 1 : 0) + (handleFreeze ? 1 : 0);
   const onButtonLayout = useCallback((e: LayoutChangeEvent) => {
@@ -72,6 +71,7 @@ const WalletButtons = ({
   }, []);
   return (
     <View
+      style={{ marginBottom: insets.bottom }}
       className={`self-center bottom-8 max-w-2xl px-2 moblg:px-4 w-full z-10 fixed native:absolute flex-wrap flex-row gap-4 ${isMultiRow ? 'justify-center' : elCount === 1 ? 'justify-center' : elCount === 2 ? 'justify-evenly' : 'justify-between'}`}
       onLayout={e => {
         const height = e.nativeEvent.layout.height;
