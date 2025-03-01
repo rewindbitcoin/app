784f7ba155afd0d0d23918ff4cb7b712f5b8cd23
José Luis Landabaso Díaz <landabaso@gmail.com>
2025-03-01 18:32:00 +0100

    style: Improve checkbox icon rendering with text base class

--- src/common/components/VerticalChoice.tsx
+++ src/common/components/VerticalChoice.tsx
@@ -29,7 +29,7 @@ const VerticalChoice = ({
     <View className="w-8 pr-2">
       {check ? (
         //Text tag needed for correct vertically-aligned rendering in iOS/Android
-        <Text>
+        <Text className="text-base">
           <MaterialCommunityIcons
             className="!text-primary text-base"
             name="checkbox-marked-circle"
@@ -37,8 +37,11 @@ const VerticalChoice = ({
         </Text>
       ) : (
         //Text tag needed for correct vertically-aligned rendering in iOS/Android
-        <Text>
-          <MaterialCommunityIcons className="!text-primary text-base" name="checkbox-blank-circle-outline" />
+        <Text className="text-base">
+          <MaterialCommunityIcons
+            className="!text-primary text-base"
+            name="checkbox-blank-circle-outline"
+          />
         </Text>
       )}
     </View>
